4d1e34587aa225f315089fa8502d67e4c1d597a7
scotow <contact@scotow.com>
2023-11-13 23:56:28 +0100

    Refactor frontend, handle clear and leave actions, proper cleanup

--- src/packet.rs
+++ src/packet.rs
@@ -1,5 +1,5 @@
-use axum::extract::ws::Message as WsMessage;
-use serde::Serialize;
+use axum::extract::ws::{Message as WsMessage, Message};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 #[derive(Serialize)]
@@ -19,5 +19,23 @@ pub enum PacketOut {
 impl From<PacketOut> for WsMessage {
     fn from(value: PacketOut) -> Self {
         Self::Text(serde_json::to_string(&value).expect("serialization failed"))
+    }
+}
+
+#[derive(Deserialize)]
+#[serde(tag = "event", rename_all(serialize = "camelCase"))]
+pub enum PacketIn {
+    Buzz,
+    Clear,
+}
+
+impl TryFrom<WsMessage> for PacketIn {
+    type Error = ();
+
+    fn try_from(value: WsMessage) -> Result<Self, Self::Error> {
+        match value {
+            Message::Text(text) => serde_json::from_str(&text).map_err(|_err| ())?,
+            _ => Err(()),
+        }
     }
 }
--- src/public/app.js
+++ src/public/app.js
@@ -1,88 +1,3 @@
-async function host() {
-    document.body.classList.replace('lobby', 'host');
-
-    const response = await fetch("/rooms", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            name: "dac"
-        }),
-    });
-    let { id, name } = await response.json();
-    console.log(id, name);
-
-    document.querySelector('.host.panel .title.panel > .labels > .label').innerText = name;
-
-    const hostSocket = new WebSocket(`ws://localhost:8080/rooms/${id}/host`);
-    hostSocket.addEventListener("message", (message) => {
-        const data = JSON.parse(message.data);
-        console.log(data);
-        switch (data.event) {
-            case 'buzzed':
-                const buzzElem = document.createElement('div');
-                buzzElem.classList.add('buzz');
-
-                const usernameElem = document.createElement('div');
-                usernameElem.classList.add('username');
-                usernameElem.innerText = data.name;
-                buzzElem.append(usernameElem);
-
-                if (data.timestampDiff !== null) {
-                    const timestampElem = document.createElement('div');
-                    timestampElem.classList.add('timestamp');
-                    if (data.timestampDiff < 1000) {
-                        timestampElem.innerText = `+${data.timestampDiff}ms`;
-                    } else if (data.timestampDiff < 10000) {
-                        timestampElem.innerText = `+${Math.floor(data.timestampDiff / 100) / 10}s`;
-                    } else {
-                        timestampElem.innerText = `+${Math.floor(data.timestampDiff / 1000)}s`;
-                    }
-                    buzzElem.append(timestampElem);
-                }
-
-                document.querySelector('.host.panel .inner.panel').append(buzzElem);
-                break;
-            case 'participantCount':
-                document.querySelector('.host.panel .title.panel > .labels > .sub-label').innerText = `${data.count} participant${data.count !== 1 ? 's' : ''}`;
-                break;
-        }
-    });
-}
-
-async function join() {
-    document.body.classList.replace('lobby', 'participate');
-
-    const response = await fetch('/rooms/id?name=dac', {
-        method: 'GET',
-    });
-    let { id, name } = await response.json();
-    console.log(id, name);
-
-    document.querySelector('.participate.panel .title.panel > .labels > .label').innerText = name;
-
-    const participateSocket = new WebSocket(`ws://localhost:8080/rooms/${id}/participate?name=Scotow`);
-    participateSocket.addEventListener('message', (message) => {
-        const data = JSON.parse(message.data);
-        console.log(data);
-        switch (data.event) {
-            case 'participantCount':
-                document.querySelector('.participate.panel .title.panel > .labels > .sub-label').innerText = `${data.count} participant${data.count !== 1 ? 's' : ''}`;
-                break;
-            case 'hostLeft':
-                alert('The host has closed the room.');
-                participateSocket.close();
-                document.body.classList.replace('participate', 'lobby');
-                break;
-        }
-    });
-
-    document.querySelector('.participate.panel .buzzer').addEventListener('click', () => {
-        participateSocket.send('buzz');
-    });
-}
-
 // let shiftOffset = 72 / 2; // Placeholder.
 // document.addEventListener('DOMContentLoaded', () => {
 //     setTimeout(() => {
@@ -108,9 +23,122 @@ document.querySelectorAll('.lobby.panel .mode').forEach((elem) => {
 document.querySelector('.lobby.panel .action').addEventListener('click', () => {
     (async function() {
         if (document.querySelector('.lobby.panel .mode.selected').classList.contains('host')) {
-            await host();
+            const response = await fetch("/rooms", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    name: document.querySelector('.lobby.panel .room.input > input').value.trim(),
+                }),
+            });
+            let { id, name } = await response.json();
+            // console.log(id, name);
+
+            run('host', name, new WebSocket(`ws://localhost:8080/rooms/${id}/host`), document.querySelector('.host.panel'));
         } else {
-            await join();
+            const response = await fetch(`/rooms/id?name=${document.querySelector('.lobby.panel .room.input > input').value.trim()}`, {
+                method: 'GET',
+            });
+            let { id, name } = await response.json();
+            run('participate', name, new WebSocket(`ws://localhost:8080/rooms/${id}/participate?name=${document.querySelector('.lobby.panel .username.input > input').value.trim()}`), document.querySelector('.participate.panel'));
         }
     })();
 });
+
+function run(mode, name, socket, panelElem) {
+    document.body.classList.replace('lobby', mode);
+    panelElem.querySelector('.title.panel > .labels > .label').innerText = name;
+
+    let buzz = 0;
+
+    socket.addEventListener('message', (message) => {
+        const data = JSON.parse(message.data);
+        switch (data.event) {
+            case 'participantCount':
+                panelElem.querySelector('.title.panel > .labels > .sub-label').innerText = `${data.count} participant${data.count !== 1 ? 's' : ''}`;
+                break;
+            case 'buzzed':
+                buzz += 1;
+
+                const buzzElem = document.createElement('div');
+                buzzElem.classList.add('buzz');
+
+                const usernameElem = document.createElement('div');
+                usernameElem.classList.add('username');
+                usernameElem.innerText = data.name;
+                buzzElem.append(usernameElem);
+
+                const rightElem = document.createElement('div');
+                rightElem.classList.add('right');
+                buzzElem.append(rightElem);
+
+                const positionElem = document.createElement('div');
+                positionElem.classList.add('position');
+                switch (buzz) {
+                    case 1:
+                        positionElem.innerText = '1st';
+                        break;
+                    case 2:
+                        positionElem.innerText = '2nd';
+                        break;
+                    case 3:
+                        positionElem.innerText = '3rd';
+                        break;
+                    default:
+                        positionElem.innerText = `${buzz}th`;
+                        break;
+                }
+                rightElem.append(positionElem);
+
+                if (data.timestampDiff !== null) {
+                    const timestampElem = document.createElement('div');
+                    timestampElem.classList.add('timestamp');
+                    if (data.timestampDiff < 1000) {
+                        timestampElem.innerText = `+${data.timestampDiff}ms`;
+                    } else if (data.timestampDiff < 10000) {
+                        timestampElem.innerText = `+${Math.floor(data.timestampDiff / 100) / 10}s`;
+                    } else {
+                        timestampElem.innerText = `+${Math.floor(data.timestampDiff / 1000)}s`;
+                    }
+                    rightElem.append(timestampElem);
+                }
+
+                panelElem.querySelector('.inner.panel').append(buzzElem);
+                break;
+            case 'hostLeft':
+                exit();
+                alert('The host has closed the room.');
+                break;
+        }
+    });
+
+    function handleBuzz() {
+        socket.send(JSON.stringify({ event: 'buzz' }));
+    }
+
+    function handleLeave() {
+        if (mode === 'participate' || confirm('You\'re about to close the room and kick every participant out. Confirm?')) {
+            exit();
+        }
+    }
+
+    function handleClear() {
+        socket.send(JSON.stringify({ event: 'clear' }));
+        buzz = 0;
+        panelElem.querySelector('.inner.panel').replaceChildren();
+    }
+
+    function exit() {
+        socket.close();
+        document.body.classList.replace(mode, 'lobby');
+        panelElem.querySelector('.inner.panel').replaceChildren();
+        panelElem.querySelector('.buzzer')?.removeEventListener('click', handleBuzz);
+        panelElem.querySelector('.leave.action')?.removeEventListener('click', handleLeave);
+        panelElem.querySelector('.clear.action')?.removeEventListener('click', handleClear);
+    }
+
+    panelElem.querySelector('.buzzer')?.addEventListener('click', handleBuzz);
+    panelElem.querySelector('.leave.action')?.addEventListener('click', handleLeave);
+    panelElem.querySelector('.clear.action')?.addEventListener('click', handleClear);
+}
